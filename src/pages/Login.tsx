e302fbc864f8ef5ff0293c3410676c00b15fdc49
powerwarez <powerware@naver.com>
2025-01-21 20:20:59 +0900

    로그인 크기 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -14,7 +14,7 @@ const Login: React.FC = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
-      <div className="text-center p-8 bg-gray-800 rounded-lg shadow-lg">
+      <div className="w-full max-w-md text-center p-8 bg-gray-800 rounded-lg shadow-lg">
         <h1 className="text-3xl mb-4">동파법 로그인</h1>
         <button
           onClick={handleLogin}
