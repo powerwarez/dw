ceb31a86f07604550cd7df4a4bd03eff9bd68d9c
powerwarez <powerware@naver.com>
2025-01-21 20:28:20 +0900

    로그인창 너비 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,15 +13,19 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
-      <div className="w-full max-w-md text-center p-8 bg-gray-800 rounded-lg shadow-lg">
-        <h1 className="text-3xl mb-4">동파법 로그인</h1>
-        <button
-          onClick={handleLogin}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          카카오로 로그인
-        </button>
+    <div className="w-full min-h-screen bg-gray-900">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="w-full max-w-md text-center p-8 bg-gray-800 rounded-lg shadow-lg text-white">
+            <h1 className="text-3xl mb-4">동파법 로그인</h1>
+            <button
+              onClick={handleLogin}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              카카오로 로그인
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
