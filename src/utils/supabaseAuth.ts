b0aebc00cf4d3078683e132e29bbace40bca70bd
powerwarez <powerware@naver.com>
2025-01-21 20:07:58 +0900

    supabase 키 수정

--- src/utils/supabaseAuth.ts
+++ src/utils/supabaseAuth.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseAuthUrl = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseAuthKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "";
+const supabaseAuthKey = import.meta.env.VITE_SUPABASE_KEY || "";
 const supabaseAuth = createClient(supabaseAuthUrl, supabaseAuthKey);
 
 export default supabaseAuth;
