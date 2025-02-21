e6096906d1cb43b1d45d2fc6e13ca99d207c7a05
powerwarez <powerware@naver.com>
2025-02-21 15:00:46 +0900

    수파베이스 수정

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string;
+const supabaseUrl = process.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.VITE_SUPABASE_API_KEY as string;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
--- src/utils/supabaseAuth.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseAuthUrl = import.meta.env.VITE_SUPABASE_URL || "";
-const supabaseAuthKey = import.meta.env.VITE_SUPABASE_KEY || "";
-const supabaseAuth = createClient(supabaseAuthUrl, supabaseAuthKey);
-
-export default supabaseAuth;
